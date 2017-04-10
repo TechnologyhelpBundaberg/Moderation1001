f368131dcc52a34bf5e08a2c88273402a240501c
Repulser <thetechproject2014@gmail.com>
2017-04-10 14:15:14 +0300

    A simple login

--- src/main/co/automod/bot/main.java
+++ src/main/co/automod/bot/main.java
@@ -1,4 +1,27 @@
 package co.automod.bot;
 
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.AnnotatedEventManager;
+
+import javax.security.auth.login.LoginException;
+
 public class main {
+    public static final String token = "";
+
+    public static void main(String[] args) {
+        JDABuilder builder = new JDABuilder(AccountType.BOT).setToken(token);
+        JDA jda;
+        try {
+            jda = builder.buildBlocking();
+            jda.setEventManager(new AnnotatedEventManager());
+            //Register events
+        } catch (LoginException | RateLimitedException | InterruptedException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+    }
 }
