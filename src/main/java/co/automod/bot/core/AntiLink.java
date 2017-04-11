375705b98cec2b126521471b9d03b1e60923f1a1
Repulser <thetechproject2014@gmail.com>
2017-04-11 21:45:18 +0300

    Perms check...

--- src/main/java/co/automod/bot/core/AntiLink.java
+++ src/main/java/co/automod/bot/core/AntiLink.java
@@ -1,19 +1,23 @@
 package co.automod.bot.core;
 
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.SubscribeEvent;
+import net.dv8tion.jda.core.utils.PermissionUtil;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AntiLink {
     private static final Pattern discordURL = Pattern.compile("discord\\.(?:me|io|gg)\\/.{0,4}\\w+|discordapp\\.com.{1,4}(?:invite|oauth2).{0,5}\\/");
+    private final Permission[] ignoredPerms = {Permission.MANAGE_SERVER, Permission.MANAGE_ROLES};
 
     @SubscribeEvent
     public void handleMessage(GuildMessageReceivedEvent e) {
         if (e.getAuthor().getId().equals(e.getJDA().getSelfUser().getId())) return;
         //Check if guild is in db and anti link is enabled.
-        //Check if he has permissions and return
+        boolean hasPerms = PermissionUtil.checkPermission(e.getGuild(), e.getMember(), ignoredPerms);
+        if (hasPerms) return;
         String content = e.getMessage().getRawContent();
         content = content.replace("\u200b", "");
         if (!content.contains("discord")) return;
