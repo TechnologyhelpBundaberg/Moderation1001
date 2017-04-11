2cc5166bf441031cb438782d70d5bfc7dd506dbb
Repulser <thetechproject2014@gmail.com>
2017-04-11 22:11:24 +0300

    Updates to antilink

--- src/main/java/co/automod/bot/core/AntiLink.java
+++ src/main/java/co/automod/bot/core/AntiLink.java
@@ -2,6 +2,7 @@ package co.automod.bot.core;
 
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageUpdateEvent;
 import net.dv8tion.jda.core.hooks.SubscribeEvent;
 import net.dv8tion.jda.core.utils.PermissionUtil;
 
@@ -12,6 +13,22 @@ public class AntiLink {
     private static final Pattern discordURL = Pattern.compile("discord\\.(?:me|io|gg)\\/.{0,4}\\w+|discordapp\\.com.{1,4}(?:invite|oauth2).{0,5}\\/");
     private final Permission[] ignoredPerms = {Permission.MANAGE_SERVER, Permission.MANAGE_ROLES};
 
+
+    @SubscribeEvent
+    public void handleEdit(GuildMessageUpdateEvent e) {
+        if (e.getAuthor().getId().equals(e.getJDA().getSelfUser().getId())) return;
+        //Check if guild is in db and anti link is enabled.
+        boolean hasPerms = PermissionUtil.checkPermission(e.getGuild(), e.getMember(), ignoredPerms);
+        if (hasPerms) return;
+        String content = e.getMessage().getRawContent();
+        content = content.replaceAll("\\p{C}", "");
+        if (!content.contains("discord")) return;
+        Matcher m = discordURL.matcher(content);
+        if (m.find()) {
+            e.getMessage().delete().queue(a -> e.getChannel().sendMessage(String.format("%s \u26D4 **Advertising is not allowed!**", e.getAuthor().getAsMention())).queue());
+        }
+    }
+
     @SubscribeEvent
     public void handleMessage(GuildMessageReceivedEvent e) {
         if (e.getAuthor().getId().equals(e.getJDA().getSelfUser().getId())) return;
@@ -19,7 +36,7 @@ public class AntiLink {
         boolean hasPerms = PermissionUtil.checkPermission(e.getGuild(), e.getMember(), ignoredPerms);
         if (hasPerms) return;
         String content = e.getMessage().getRawContent();
-        content = content.replace("\u200b", "");
+        content = content.replaceAll("\\p{C}", "");
         if (!content.contains("discord")) return;
         Matcher m = discordURL.matcher(content);
         if (m.find()) {
