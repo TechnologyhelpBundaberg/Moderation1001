8d76f47880c1d2e77aba13b0a6c3ad9406549745
Maik Wezinkhof <maik_wezinkhof@hotmail.com>
2017-04-12 12:37:41 +0200

    help command sorted + cached based on category

--- src/main/java/co/automod/bot/commands/info/HelpCommand.java
+++ src/main/java/co/automod/bot/commands/info/HelpCommand.java
@@ -2,15 +2,20 @@ package co.automod.bot.commands.info;
 
 import co.automod.bot.core.listener.CommandListener;
 import co.automod.bot.core.listener.command.Command;
+import co.automod.bot.core.listener.command.CommandCategory;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeSet;
 
 public class HelpCommand extends Command {
+    private String fullHelpText = null;
+
     @Override
     public String getTrigger() {
         return "help";
@@ -36,19 +41,40 @@ public class HelpCommand extends Command {
 
     @Override
     public void execute(Guild guild, TextChannel channel, User invoker, Message message, String args) {
-        StringBuilder desc = new StringBuilder();
-        for (Map.Entry<String, Command> entry : CommandListener.commands.entrySet()) {
+        if (fullHelpText == null) {
+            createHelpCache();
+        }
+        invoker.openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage(new EmbedBuilder()
+                .setAuthor("AutoMod Commands!", null, channel.getJDA().getSelfUser().getAvatarUrl())
+                .setDescription(fullHelpText)
+                .setFooter("AutoMod ready at your command!", null)
+                .build()).queue());
+    }
 
+    private void createHelpCache() {
+        StringBuilder sb = new StringBuilder();
+        HashMap<CommandCategory, TreeSet<String>> cmds = new HashMap<>();
+        for (Map.Entry<String, Command> entry : CommandListener.commands.entrySet()) {
             String key = entry.getKey();
             Command value = entry.getValue();
             if (!value.isListed()) continue;
-            desc.append(String.format("[!%s](https://github.com/repulser/automod) - %s\n\n", key, value.getDescription()));
+            if (!cmds.containsKey(value.getCategory())) {
+                cmds.put(value.getCategory(), new TreeSet<>());
+            }
+            cmds.get(value.getCategory()).add(value.getTrigger());
         }
-        final String newDescription = desc.toString();
-        invoker.openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage(new EmbedBuilder()
-                .setAuthor("AutoMod Commands!", null, channel.getJDA().getSelfUser().getAvatarUrl())
-                .setDescription(newDescription)
-                .setFooter("AutoMod ready at your command!", null)
-                .build()).queue());
+        for (CommandCategory category : CommandCategory.values()) {
+            if (!cmds.containsKey(category)) {
+                continue;
+            }
+            TreeSet<String> set = cmds.get(category);
+            if (set.isEmpty()) {
+                continue;
+            }
+            for (String trigger : set) {
+                sb.append(String.format("[!%s](https://github.com/repulser/automod) - %s\n\n", trigger, CommandListener.commands.get(trigger).getDescription()));
+            }
+        }
+        fullHelpText = sb.toString();
     }
 }
