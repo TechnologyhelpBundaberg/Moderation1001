8244fbd09e21669dcc8094b5c8e0b69273662e1a
Repulser <thetechproject2014@gmail.com>
2017-04-13 11:02:12 +0300

    Cleanup modlog :smile:

--- src/main/java/co/automod/bot/core/ModLog.java
+++ src/main/java/co/automod/bot/core/ModLog.java
@@ -3,10 +3,7 @@ package co.automod.bot.core;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.rethinkdb.gen.exc.ReqlNonExistenceError;
-import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.TextChannel;
-import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.guild.GuildBanEvent;
 import net.dv8tion.jda.core.events.guild.member.*;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageDeleteEvent;
@@ -40,20 +37,45 @@ public class ModLog {
         }
     }
 
-    @SubscribeEvent
-    public void onDelete(GuildMessageDeleteEvent e) {
-        String tempChannel;
+    private String getChannel(Guild guild) {
         try {
-            tempChannel = r.table("modlog").get(e.getGuild().getId()).getField("channelId").run(conn);
+            return r.table("modlog").get(guild.getId()).getField("channelId").run(conn);
 
         } catch (ReqlNonExistenceError ignored) {
-            return;
+            return null;
         }
+    }
+
+    private void deleteGuild(Guild guild) {
+
+        r.table("modlog").get(guild.getId()).delete().runNoReply(conn);
+    }
+
+    private String getTime() {
+        return dateFormat.format(new Date());
+    }
+
+    private String getUser(Member member) {
+        return member.getEffectiveName() + "#" + member.getUser().getDiscriminator();
+    }
+
+    private Message getFromCache(String id) {
+        try {
+            return MessageCache.get(id, Optional::empty).orElse(null);
+        } catch (ExecutionException e1) {
+            e1.printStackTrace();
+            return null;
+        }
+    }
+
+    @SubscribeEvent
+    public void onDelete(GuildMessageDeleteEvent e) {
+        String tempChannel = getChannel(e.getGuild());
         if (tempChannel == null) return;
         TextChannel channel = e.getJDA().getTextChannelById(tempChannel);
         if (channel == null) {
             System.out.println("Could not find channel for id " + tempChannel);
-            r.table("modlog").get(e.getGuild().getId()).delete().runNoReply(conn);
+            deleteGuild(e.getGuild());
             return;
         }
         if (!e.getGuild().getId().equals(channel.getGuild().getId())) return;
@@ -67,95 +89,66 @@ public class ModLog {
             e1.printStackTrace();
         }
         Message DeletedMessage;
-        try {
-            DeletedMessage = MessageCache.get(e.getMessageId(), Optional::empty).orElse(null);
-        } catch (ExecutionException e1) {
-            e1.printStackTrace();
-            return;
-        }
+        DeletedMessage = getFromCache(e.getMessageId());
         if (DeletedMessage == null) return;
-        String time = dateFormat.format(new Date());
+        String time = getTime();
         Member author = DeletedMessage.getGuild().getMember(DeletedMessage.getAuthor());
-        String user = author.getEffectiveName() + "#" + author.getUser().getDiscriminator();
+        String user = getUser(author);
         channel.sendMessage(String.format("\uD83D\uDCDD `[%s]` %s **%s's** message has been deleted `%s`", time, e.getChannel().getAsMention(), user, DeletedMessage.getStrippedContent())).queue(msg -> SelfCache.put(msg.getId(), Optional.of(msg.getRawContent())));
     }
 
     @SubscribeEvent
     public void onEdit(GuildMessageUpdateEvent e) {
-        String tempChannel;
-        try {
-            tempChannel = r.table("modlog").get(e.getGuild().getId()).getField("channelId").run(conn);
-
-        } catch (ReqlNonExistenceError ignored) {
-            return;
-        }
+        String tempChannel = getChannel(e.getGuild());
         if (tempChannel == null) return;
         TextChannel channel = e.getJDA().getTextChannelById(tempChannel);
         if (channel == null) {
             System.out.println("Could not find channel for id " + tempChannel);
-            r.table("modlog").get(e.getGuild().getId()).delete().runNoReply(conn);
+            deleteGuild(e.getGuild());
             return;
         }
         if (!e.getGuild().getId().equals(channel.getGuild().getId())) return;
         Message after = e.getMessage();
-        Message before;
-        try {
-            before = MessageCache.get(after.getId(), Optional::empty).orElse(null);
-        } catch (ExecutionException e1) {
-            e1.printStackTrace();
-            return;
-        }
+        Message before = getFromCache(after.getId());
         if (before == null || after == null) return;
-        String time = dateFormat.format(new Date());
+        String time = getTime();
         Member author = before.getGuild().getMember(before.getAuthor());
-        String user = author.getEffectiveName() + "#" + author.getUser().getDiscriminator();
+        String user = getUser(author);
         channel.sendMessage(String.format("\uD83D\uDCDD `[%s]` %s **%s's** message has been edited\n\nBefore: `%s`\n\nAfter: `%s`", time, e.getChannel().getAsMention(), user, before.getStrippedContent(), after.getStrippedContent())).queue(msg -> SelfCache.put(msg.getId(), Optional.of(msg.getRawContent())));
 
     }
 
     @SubscribeEvent
     public void onLeave(GuildMemberLeaveEvent e) {
-        String tempChannel;
-        try {
-            tempChannel = r.table("modlog").get(e.getGuild().getId()).getField("channelId").run(conn);
-
-        } catch (ReqlNonExistenceError ignored) {
-            return;
-        }
+        String tempChannel = getChannel(e.getGuild());
         if (tempChannel == null) return;
         TextChannel channel = e.getJDA().getTextChannelById(tempChannel);
         if (channel == null) {
             System.out.println("Could not find channel for id " + tempChannel);
-            r.table("modlog").get(e.getGuild().getId()).delete().runNoReply(conn);
+            deleteGuild(e.getGuild());
             return;
         }
         if (!e.getGuild().getId().equals(channel.getGuild().getId())) return;
-        String time = dateFormat.format(new Date());
+        String time = getTime();
         Member author = e.getMember();
-        String user = author.getEffectiveName() + "#" + author.getUser().getDiscriminator();
+        String user = getUser(author);
         channel.sendMessage(String.format("\u274C `[%s]` **%s** has left the server or was kicked. Total members `%s`", time, user, e.getGuild().getMembers().size())).queue(msg -> SelfCache.put(msg.getId(), Optional.of(msg.getRawContent())));
     }
 
     @SubscribeEvent
     public void onJoin(GuildMemberJoinEvent e) {
-        String tempChannel;
-        try {
-            tempChannel = r.table("modlog").get(e.getGuild().getId()).getField("channelId").run(conn);
-
-        } catch (ReqlNonExistenceError ignored) {
-            return;
-        }
+        String tempChannel = getChannel(e.getGuild());
         if (tempChannel == null) return;
         TextChannel channel = e.getJDA().getTextChannelById(tempChannel);
         if (channel == null) {
             System.out.println("Could not find channel for id " + tempChannel);
-            r.table("modlog").get(e.getGuild().getId()).delete().runNoReply(conn);
+            deleteGuild(e.getGuild());
             return;
         }
         if (!e.getGuild().getId().equals(channel.getGuild().getId())) return;
-        String time = dateFormat.format(new Date());
+        String time = getTime();
         Member author = e.getMember();
-        String user = author.getEffectiveName() + "#" + author.getUser().getDiscriminator();
+        String user = getUser(author);
         long days = Duration.between(author.getUser().getCreationTime(), OffsetDateTime.now()).toDays();
         String created = days > 10 ? String.format("**Created %s days ago.**", days) : String.format("\u26A0 **New User - joined %s days ago.**", days);
         channel.sendMessage(String.format("\u2705 `[%s]` **%s** joined the server. %s Total members `%s`", time, user, created, e.getGuild().getMembers().size())).queue(msg -> SelfCache.put(msg.getId(), Optional.of(msg.getRawContent())));
@@ -163,22 +156,16 @@ public class ModLog {
 
     @SubscribeEvent
     public void onBan(GuildBanEvent e) {
-        String tempChannel;
-        try {
-            tempChannel = r.table("modlog").get(e.getGuild().getId()).getField("channelId").run(conn);
-
-        } catch (ReqlNonExistenceError ignored) {
-            return;
-        }
+        String tempChannel = getChannel(e.getGuild());
         if (tempChannel == null) return;
         TextChannel channel = e.getJDA().getTextChannelById(tempChannel);
         if (channel == null) {
             System.out.println("Could not find channel for id " + tempChannel);
-            r.table("modlog").get(e.getGuild().getId()).delete().runNoReply(conn);
+            deleteGuild(e.getGuild());
             return;
         }
         if (!e.getGuild().getId().equals(channel.getGuild().getId())) return;
-        String time = dateFormat.format(new Date());
+        String time = getTime();
         User author = e.getUser();
         String user = author.getName() + "#" + author.getDiscriminator();
         channel.sendMessage(String.format("\uD83D\uDD28 `[%s]` **%s**(%s) has been banned. Total members `%s`", time, user, author.getId(), e.getGuild().getMembers().size())).queue(msg -> SelfCache.put(msg.getId(), Optional.of(msg.getRawContent())));
@@ -187,71 +174,53 @@ public class ModLog {
 
     @SubscribeEvent
     public void onNickChange(GuildMemberNickChangeEvent e) {
-        String tempChannel;
-        try {
-            tempChannel = r.table("modlog").get(e.getGuild().getId()).getField("channelId").run(conn);
-
-        } catch (ReqlNonExistenceError ignored) {
-            return;
-        }
+        String tempChannel = getChannel(e.getGuild());
         if (tempChannel == null) return;
         TextChannel channel = e.getJDA().getTextChannelById(tempChannel);
         if (channel == null) {
             System.out.println("Could not find channel for id " + tempChannel);
-            r.table("modlog").get(e.getGuild().getId()).delete().runNoReply(conn);
+            deleteGuild(e.getGuild());
             return;
         }
         if (!e.getGuild().getId().equals(channel.getGuild().getId())) return;
-        String time = dateFormat.format(new Date());
+        String time = getTime();
         Member author = e.getMember();
-        String user = author.getEffectiveName() + "#" + author.getUser().getDiscriminator();
+        String user = getUser(author);
         channel.sendMessage(String.format("\u2705 `[%s]` **%s** Changed his name\n\nBefore: `%s`\n\nAfter: `%s`", time, user, e.getPrevNick() == null ? e.getMember().getUser().getName() : e.getPrevNick(), e.getNewNick() == null ? e.getMember().getUser().getName() : e.getNewNick())).queue(msg -> SelfCache.put(msg.getId(), Optional.of(msg.getRawContent())));
     }
 
     @SubscribeEvent
     public void roleAdd(GuildMemberRoleAddEvent e) {
-        String tempChannel;
-        try {
-            tempChannel = r.table("modlog").get(e.getGuild().getId()).getField("channelId").run(conn);
-
-        } catch (ReqlNonExistenceError ignored) {
-            return;
-        }
+        String tempChannel = getChannel(e.getGuild());
         if (tempChannel == null) return;
         TextChannel channel = e.getJDA().getTextChannelById(tempChannel);
         if (channel == null) {
             System.out.println("Could not find channel for id " + tempChannel);
-            r.table("modlog").get(e.getGuild().getId()).delete().runNoReply(conn);
+            deleteGuild(e.getGuild());
             return;
         }
         if (!e.getGuild().getId().equals(channel.getGuild().getId())) return;
-        String time = dateFormat.format(new Date());
+        String time = getTime();
         Member author = e.getMember();
-        String user = author.getEffectiveName() + "#" + author.getUser().getDiscriminator();
+        String user = getUser(author);
         String AddedRoles = String.join(", ", e.getRoles().stream().map(r -> r.getName()).collect(Collectors.toList()));
         channel.sendMessage(String.format("\u2611 `[%s]` a role has been added to **%s** - `%s`", time, user, AddedRoles)).queue(msg -> SelfCache.put(msg.getId(), Optional.of(msg.getRawContent())));
     }
 
     @SubscribeEvent
     public void roleRemove(GuildMemberRoleRemoveEvent e) {
-        String tempChannel;
-        try {
-            tempChannel = r.table("modlog").get(e.getGuild().getId()).getField("channelId").run(conn);
-
-        } catch (ReqlNonExistenceError ignored) {
-            return;
-        }
+        String tempChannel = getChannel(e.getGuild());
         if (tempChannel == null) return;
         TextChannel channel = e.getJDA().getTextChannelById(tempChannel);
         if (channel == null) {
             System.out.println("Could not find channel for id " + tempChannel);
-            r.table("modlog").get(e.getGuild().getId()).delete().runNoReply(conn);
+            deleteGuild(e.getGuild());
             return;
         }
         if (!e.getGuild().getId().equals(channel.getGuild().getId())) return;
-        String time = dateFormat.format(new Date());
+        String time = getTime();
         Member author = e.getMember();
-        String user = author.getEffectiveName() + "#" + author.getUser().getDiscriminator();
+        String user = getUser(author);
         String AddedRoles = String.join(", ", e.getRoles().stream().map(r -> r.getName()).collect(Collectors.toList()));
         channel.sendMessage(String.format("\u274C `[%s]` a role has been removed from **%s** - `%s`", time, user, AddedRoles)).queue(msg -> SelfCache.put(msg.getId(), Optional.of(msg.getRawContent())));
     }
