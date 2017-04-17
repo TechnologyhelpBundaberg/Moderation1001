295691bc80081ff8d867f6ba69878b2e8cd02796
Repulser <thetechproject2014@gmail.com>
2017-04-17 08:40:03 +0300

    Fixes to rethinkdb

--- src/main/java/co/automod/bot/core/Rethink.java
+++ src/main/java/co/automod/bot/core/Rethink.java
@@ -12,7 +12,7 @@ public class Rethink {
     public static void init() {
         connection = r.connection().hostname(Config.rethink_host).port(Config.rethink_port).db(Config.rethink_db_name).connect();
         if (!(boolean) r.dbList().contains(Config.rethink_db_name).run(connection)) {
-            r.dbCreate(Config.rethink_db_name).run(connection);
+            r.dbCreate(Config.rethink_db_name).runNoReply(connection);
         }
         String[] requiredTables = {"modlog", "antilink", "prefixes"};
         for (String tbl : requiredTables) {
@@ -22,7 +22,7 @@ public class Rethink {
 
     public static void createTable(String tableName) {
         if (!(boolean) r.db(Config.rethink_db_name).tableList().contains(tableName).run(connection)) {
-            r.db(Config.rethink_db_name).tableCreate(tableName).run(connection);
+            r.db(Config.rethink_db_name).tableCreate(tableName).runNoReply(connection);
         }
     }
 
@@ -31,6 +31,6 @@ public class Rethink {
     }
 
     public static void saveConfigField(String table, Object data) {
-        r.table(table).insert(data).optArg("conflict", "replace").run(connection);
+        r.table(table).insert(data).optArg("conflict", "replace").runNoReply(connection);
     }
 }
